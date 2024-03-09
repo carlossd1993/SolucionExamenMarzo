266f7c479aa0275e22aca668cf2ea80157cdafdd
carlossd1993 <161085364+carlossd1993@users.noreply.github.com>
2024-03-09 19:47:09 +0100

    Terminado Menu

--- src/Pages/Components/Menu.jsx
+++ src/Pages/Components/Menu.jsx
@@ -1,5 +1,5 @@
 import {Navbar, Container, Nav} from "react-bootstrap";
-import {Link, NavLink} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 function Menu(){
 
@@ -10,10 +10,9 @@ function Menu(){
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="./">Home</Nav.Link>
-                        <Nav.Link href="./Obras">Obras</Nav.Link>
-                        <Nav.Link href="./Contacto">Contacto</Nav.Link>
-
+                        <Link className="nav-link" to="./">Home</Link>
+                        <Link className="nav-link" to="./Obras">Obras</Link>
+                        <Link className="nav-link" to="./Contacto">Contacto</Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
